2084b6e361044437399396b3045ac4444a9fe541
Breno do Amaral <brenodoamaral1@gmail.com>
2024-11-30 06:04:31 -0300

    Mapa de Alerta

---
+++ components/Mapa.tsx
@@ -0,0 +1,279 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
+import AlertaPin from '../assets/AlertaPin.svg'
+
+interface InputFields {
+  titulo: string;
+  outraCategoria: string;
+  localizacao: string;
+}
+
+interface AdicionarAlertaProps {
+  navigation: any; // Altere para o tipo correto se você estiver usando react-navigation
+}
+
+const MapaAlerta: React.FC<AdicionarAlertaProps> = ({ navigation }) => {
+  const [category, setCategory] = useState<string>("");
+  const [inputs, setInputs] = useState<InputFields>({
+    titulo: "",
+    outraCategoria: "",
+    localizacao: "",
+  });
+
+  const handleCategoryChange = (newCategory: string) => {
+    setCategory(newCategory);
+  };
+
+  const handleInputChange = (name: keyof InputFields, value: string) => {
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    // Reset dos campos após envio
+    setCategory("");
+    setInputs({
+      titulo: "",
+      outraCategoria: "",
+      localizacao: "",
+    });
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <AlertaPin />
+        <Text style={styles.title}>Mapa de Alerta</Text>
+      </View>
+
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: -16.68919316138317,
+          longitude: -49.273407485741714,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude:-16.713129216448422, longitude: -49.28718162582892 }}
+          title="Localização"
+          description="Local do alerta"
+        >
+          <View style={styles.marker}>
+            <Text style={styles.markerText}>⚠️</Text>
+          </View>
+        </Marker>
+      </MapView>
+
+
+      <Text style={styles.subTitle}>Categoria</Text>
+      <View style={styles.categoryContainer}>
+        <TouchableOpacity
+          style={[
+            styles.categoryButton,
+            category === "Queimada" ? styles.selectedCategory : null
+          ]}
+          onPress={() => handleCategoryChange("Queimada")}
+        >
+          <Text
+            style={[
+              styles.categoryText,
+              category === "Queimada" ? styles.selectedCategoryText : null
+            ]}
+          >
+            Queimada
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.categoryButton,
+            category === "Poluição" ? styles.selectedCategory : null
+          ]}
+          onPress={() => handleCategoryChange("Poluição")}
+        >
+          <Text
+            style={[
+              styles.categoryText,
+              category === "Poluição" ? styles.selectedCategoryText : null
+            ]}
+          >
+            Poluição
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.categoryButton,
+            category === "Outros" ? styles.selectedCategory : null
+          ]}
+          onPress={() => handleCategoryChange("Outros")}
+        >
+          <Text
+            style={[
+              styles.categoryText,
+              category === "Outros" ? styles.selectedCategoryText : null
+            ]}
+          >
+            Outros
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {category === "Outros" ? (
+        <>
+          <Text style={styles.subTitle}>Outra categoria</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Ex: Deslizamento"
+            placeholderTextColor="#808080"
+            value={inputs.outraCategoria}
+            onChangeText={(text) => handleInputChange("outraCategoria", text)}
+          />
+        </>
+      ) : null }
+
+      <Text style={styles.subTitle}>Localização</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Rua, Bairro, Cidade"
+        placeholderTextColor="#808080"
+        value={inputs.localizacao}
+        onChangeText={(text) => handleInputChange("localizacao", text)}
+      />
+
+      <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+        <Text style={styles.submitText}>Enviar</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  backIcon: {
+    marginLeft: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginVertical: 16,
+    marginLeft: 10,
+    color: "rgba(176, 35, 4, 1)",
+  },
+  uploadContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    height: 150,
+    backgroundColor: "rgba(142, 188, 218, 0.2)",
+    alignItems: "center",
+    marginBottom: 20,
+    borderRadius: 30,
+  },
+  image: {
+    width: 230,
+    height: 110,
+    marginRight: 20,
+    borderRadius: 20,
+  },
+  subTitle: {
+    fontSize: 16,
+    color: "#4D4D4D",
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  input: {
+    height: 50,
+    backgroundColor: "#fff",
+    borderRadius: 50,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  categoryContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  categoryButton: {
+    flex: 1,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    marginHorizontal: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  selectedCategory: {
+    backgroundColor: "rgba(176, 35, 4, 1)",
+  },
+  categoryText: {
+    color: "#808080",
+  },
+  selectedCategoryText: {
+    color: "#fff",
+  },
+  map: {
+    marginTop: 10,
+    width: "100%",
+    height: 250,
+    marginBottom: 20,
+  },
+  marker: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(255, 0, 0, 0.7)",
+    borderRadius: 20,
+    padding: 5,
+  },
+  markerText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  submitButton: {
+    height: 40,
+    backgroundColor: "rgba(176, 35, 4, 1)",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginBottom: 20,
+    marginTop: 10,
+    marginHorizontal: 110,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  submitText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default MapaAlerta;
--- src/pages/EcoAjuda/index.tsx
+++ src/pages/EcoAjuda/index.tsx
@@ -1,109 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../../components/Header";
 import {
   View,
   Text,
-  TextInput,
   StyleSheet,
   TouchableOpacity,
-  FlatList,
-  Image,
   ScrollView,
 } from "react-native";
-import { Feather } from '@expo/vector-icons';
+import Campanha from "../../../components/Campanhas";
+import Mapa from "../../../components/Mapa";
 
 const App: React.FC = () => {
-  const campaigns = [
-    {
-      id: "1",
-      image: "https://media.licdn.com/dms/image/v2/C5612AQGxNIechRff9w/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1634725836954?e=2147483647&v=beta&t=WJDcF7yhWYAdoI006uaP9E9X75NAAr-KGQrjVB3WM-Y",
-      title: "Ajude a levar água para a comunidade",
-      raised: "R$10.000",
-      goal: "R$50.000",
-      progress: 10,
-    },
-    {
-      id: "2",
-      image: "https://h2ahubagroambiental.com.br/wp-content/uploads/2023/11/pinheiros-lindos-nas-montanhas-1.jpg",
-      title: "Viva o verde: replantio de árvores",
-      raised: "R$25.000",
-      goal: "R$60.000",
-      progress: 40,
-    },
-  ];
+  const [activeComponent, setActiveComponent] = useState("Campanha"); // Estado para controlar o componente ativo
 
   return (
     <ScrollView style={styles.container}>
-      
       <Header />
       <Text style={styles.headerText}>Bem-vindo ao portal EcoAjuda</Text>
+
+      {/* Botões de navegação */}
       <View style={styles.buttonRow}>
-        <TouchableOpacity style={[styles.navButton, styles.activeButton]}>
-          <Text style={[styles.navButtonText, styles.activeButtonText]}>Campanhas</Text>
+        <TouchableOpacity
+          style={[
+            styles.navButton,
+            activeComponent === "Campanha" && styles.activeButton,
+          ]}
+          onPress={() => setActiveComponent("Campanha")}
+        >
+          <Text
+            style={[
+              styles.navButtonText,
+              activeComponent === "Campanha" && styles.activeButtonText,
+            ]}
+          >
+            Campanhas
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.navButton}>
-          <Text style={styles.navButtonText}>Mapa de alerta</Text>
+        <TouchableOpacity
+          style={[
+            styles.navButton,
+            activeComponent === "Mapa" && styles.activeButton,
+          ]}
+          onPress={() => setActiveComponent("Mapa")}
+        >
+          <Text
+            style={[
+              styles.navButtonText,
+              activeComponent === "Mapa" && styles.activeButtonText,
+            ]}
+          >
+            Mapa de alerta
+          </Text>
         </TouchableOpacity>
       </View>
 
-      {/* Barra de busca */}
-      <View style={styles.searchContainer}>
-      <Feather name="search" size={20} color="#6c757d" />
-        <TextInput
-          placeholder="Buscar campanhas"
-          style={styles.searchInput}
-        />
-        
+      {/* Renderização condicional do componente */}
+      <View>
+        {activeComponent === "Campanha" && <Campanha />}
+        {activeComponent === "Mapa" && <Mapa navigation={undefined} />}
       </View>
-
-      {/* Lista de campanhas */}
-      <FlatList
-        data={campaigns}
-        keyExtractor={(item) => item.id}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <View style={styles.campaignCard}>
-            <Image source={{ uri: item.image }} style={styles.campaignImage} />
-            <Text style={styles.campaignTitle}>{item.title}</Text>
-            <Text style={styles.campaignDetails}>
-             <Text style={styles.raisedAmount}>{item.raised}</Text> de {item.goal}
-            </Text>
-            <View style={styles.progressBar}>
-              <View
-                style={[
-                  styles.progress,
-                  { width: `${item.progress}%` },
-                ]}
-              />
-            </View>
-          </View>
-        )}
-      />
-
-      {/* Minhas campanhas */}
-      <Text style={styles.sectionTitle}>Minhas campanhas</Text>
-<View style={styles.emptyCampaignContainer}>
-  {/* Fantasminha e conteúdo */}
-  <Image
-    source={require('../../../assets/ghost.png')}
-    style={styles.emptyCampaignImage}
-  />
-  <View style={styles.emptyCampaignContent}>
-    <Text style={styles.emptyCampaignText}>Você ainda não criou</Text>
-    <TouchableOpacity style={styles.createButton}>
-  <View style={styles.createButtonContent}>
-    <Image
-      source={require('../../../assets/plus.png')}
-      style={styles.createButtonImage}
-    />
-    <Text style={styles.createButtonText}>Criar campanha</Text>
-  </View>
-</TouchableOpacity>
-
-  </View>
-</View>
-
     </ScrollView>
   );
 };
@@ -119,7 +74,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     marginBottom: 16,
-    marginTop: 26
+    marginTop: 26,
   },
   buttonRow: {
     flexDirection: "row",
@@ -132,7 +87,9 @@ const styles = StyleSheet.create({
     borderRadius: 7,
     backgroundColor: "#eaf0ec",
     marginHorizontal: 7,
-    width: 150
+    justifyContent: 'center',
+    width: 150,
+    height: 50,
   },
   activeButton: {
     backgroundColor: "#4C97BF",
@@ -140,71 +97,13 @@ const styles = StyleSheet.create({
   navButtonText: {
     fontSize: 14,
     color: "#333",
-    textAlign: 'center'
+    textAlign: "center", 
+    alignSelf: 'center',
   },
   activeButtonText: {
     fontWeight: "bold",
     color: "#fff",
-    textAlign: 'center'
-  },
-  searchContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#BFE7E0",
-    borderRadius: 35,
-    paddingHorizontal: 12,
-    paddingVertical: 8,
-    marginBottom: 16,
-    
-  },
-  searchInput: {
-    flex: 1,
-    fontSize: 16,
-    color: "#333",
-    justifyContent: 'center'
-  },
-  campaignCard: {
-    backgroundColor: "#ececec",
-    borderRadius: 8,
-    marginRight: 16,
-    padding: 2,
-    width: 200,
-    overflow: 'hidden'
-  },
-  campaignImage: {
-    width: "100%",
-    height: 100,
-    borderRadius: 8,
-    marginBottom: 8,
-    overflow: "hidden",   
-    resizeMode: "cover",
-  },
-  campaignTitle: {
-    fontSize: 14,
-    fontWeight: "bold",
-    marginBottom: 4,
-    padding: 7
-  },
-  campaignDetails: {
-    fontSize: 12,
-    color: "#555",
-    marginBottom: 8,
-    padding: 7
-  },
-  raisedAmount: {
-    color: "#4C97BF",
-    fontWeight: "bold", 
-  },
-  
-  progressBar: {
-    height: 6,
-    borderRadius: 3,
-    backgroundColor: "#eaf0ec",
-    overflow: "hidden",
-  },
-  progress: {
-    height: "100%",
-    backgroundColor: "#4C97BF",
+    textAlign: "center",
   },
   sectionTitle: {
     fontSize: 16,
@@ -212,52 +111,6 @@ const styles = StyleSheet.create({
     marginTop: 24,
     marginBottom: 8,
   },
-  emptyCampaignContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: 'flex-start',
-    padding: 16,
-    backgroundColor: "#f5f5f5",
-    borderRadius: 8,
-    marginBottom: 26
-  },
-  emptyCampaignImage: {
-    width: 80,
-    height: 80,
-    marginBottom: 12,
-  },
-  emptyCampaignContent: {
-    flex: 1, // Faz o conteúdo ocupar o restante do espaço
-  },
-  emptyCampaignText: {
-    fontSize: 14,
-    color: "#555",
-    marginBottom: 8,
-  },
-  createButton: {
-    flexDirection: "row", 
-    alignItems: "center", 
-    backgroundColor: "#fff",
-    borderRadius: 10, 
-    padding: 3,
-    alignSelf: "flex-start", 
-  },
-  createButtonContent: {
-    flexDirection: "row", 
-    alignItems: "flex-start", 
-    marginTop: 11
-  },
-  createButtonImage: {
-    width: 49, 
-    height: 35, 
-  },
-  createButtonText: {
-    color: "#4C97BF", 
-    fontWeight: "bold",
-    fontSize: 14,
-    marginRight: 7
-  },
-  
 });
 
 export default App;
