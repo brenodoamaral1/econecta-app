8d7cfd29674664c36c25e1021512a7be8584d319
Breno do Amaral <brenodoamaral1@gmail.com>
2024-11-30 02:48:20 -0300

    ComunidadeConectada + Carousel

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,21 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import React from 'react'
-import Header from '../../../components/Header'
-import HomeHeader from '../../../components/HomeHeader'
-import AprendeMais from '../../../components/AprendeMais'
+import React from "react";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, FlatList, Image, SafeAreaView, ScrollView  } from "react-native";
+import Header from "../../../components/Header";
+import HomeHeader from "../../../components/HomeHeader"; 
+import ComunidadeConectada from '../../../components/ComunidadeConectada'
 
+const Home: React.FC = () => {
 
-const Home = () => {
-  return (
-  <ScrollView>
-    <View style={styles.container}>
-      <View>
-        <Header />
-      </View>
-      <View>
-        <HomeHeader />
-      </View>
-      <View>
-        <AprendeMais />
-      </View>
-    </View>
-  </ScrollView>  
-  )
-}
+  return (  
+    <ScrollView style={styles.container}>
+      
+      <Header />
+
+      <HomeHeader />
+
+    </ScrollView> 
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
