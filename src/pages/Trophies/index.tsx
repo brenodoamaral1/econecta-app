56003ccc592e702e0cb3cd1b99ca64299b0053d6
Breno do Amaral <brenodoamaral1@gmail.com>
2024-11-30 06:13:04 -0300

    Página Trofeus

--- src/pages/Trophies/index.tsx
+++ src/pages/Trophies/index.tsx
@@ -1,14 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 
-const Trophies = () => {
+const ImpactoAmbiental: React.FC = () => {
   return (
-    <View>
-      <Text>Trophies</Text>
-    </View>
-  )
-}
+    <ScrollView style={styles.container}>
+      <Text style={styles.header}>Seu impacto no meio ambiente</Text>
 
-export default Trophies
+      <View style={styles.impactContainer}>
+        <View style={styles.impactCard}>
+          <Text style={styles.impactTitle}>Materiais Reutilizados</Text>
+          <Text style={styles.impactDescription}>12.5kg de resíduos reciclados.</Text>
+        </View>
 
-const styles = StyleSheet.create({})
+        <View style={styles.impactCard}>
+          <Text style={styles.impactTitle}>Redução de Emissões de CO₂</Text>
+          <Text style={styles.impactDescription}>8.0kg de CO₂ evitados.</Text>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#fff",
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 16,
+    textAlign: "center",
+  },
+  impactContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+  impactCard: {
+    flex: 1,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 16,
+    marginHorizontal: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  impactTitle: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginBottom: 16,
+    textAlign: "center",
+  },
+  impactDescription: {
+    fontSize: 12,
+    color: "#4D4D4D",
+    marginTop: 8,
+    textAlign: "center",
+  },
+});
+
+export default ImpactoAmbiental;
